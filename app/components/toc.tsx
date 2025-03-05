8dec4795505f640740c7b5332414527cf06f83c3
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-03-05 16:20:43 -0600

    toc scrolling and style improvements

--- app/components/toc.tsx
+++ app/components/toc.tsx
@@ -16,8 +16,8 @@ export const TOC: FC<{ toc: Heading[] }> = ({ toc }) => {
   }
 
   return (
-    <aside className="hidden md:block fixed top-4 right-4 mt-20 p-4 bg-white dark:bg-gray-800 shadow-lg rounded">
-      <div style={{ background: '#222222', padding: 20 }}>
+    <aside className="hidden md:block p-4 pt-12 dark:bg-gray-800 rounded">
+      <div className='sticky top-20'>
         <h2 className="font-bold pb-2 text-lg">In this post</h2>
         <ul>
           {toc.map(heading => (
