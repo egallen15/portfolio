721de66b8c20bc29e22a2a7dbad8eaf4036f2acb
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-04-30 15:35:53 -0500

    text color tweaks

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,16 +6,16 @@ export const Footer: FC = () => {
 
   return (
     <footer>
-      <div className="w-full max-w-screen-xl mx-auto p-6 mb-6 md:py-8">
-        <hr className="mb-6 border-gray-200 dark:border-slate-700 sm:mx-auto lg:my-8" />
+      <div className="w-full max-w-screen-xl mx-auto mb-6 md:py-8">
+        <hr className="mb-6 border-slate-200 dark:border-slate-700 sm:mx-auto lg:my-8" />
         <div className="relative group w-full">
           <button
-            className="inline-block hover:animate-lawnmower active:animate-lawnmower hover:text-green-500 hover:neon text-gray-500 dark:text-gray-400 mr-1 absolute transition-transform duration-1000 z-10"
+            className="inline-block hover:animate-lawnmower active:animate-lawnmower hover:text-green-500 hover:neon text-slate-500 mr-1 absolute transition-transform duration-1000 z-10"
             style={{ left: 0 }}
           >
             \.=.
           </button>
-          <span className="inline-block text-sm ml-7 text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="inline-block text-sm ml-7 text-slate-500 sm:text-center">
            © {new Date().getFullYear()}{" "}
           <a href="https://ericallenux.com" className="hover:underline">
             Eric Allen UX
