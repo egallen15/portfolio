72d867b8c996fe40238b62d6ef27f5315ec9e763
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-07-02 22:14:08 -0500

    fix: portfolio heading text on dark mode

--- app/components/BentoGrid.tsx
+++ app/components/BentoGrid.tsx
@@ -5,7 +5,7 @@ export default function BentoGrid() {
         <div className="py-24 sm:py-32">
           <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
             <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-            <p className="mt-2 max-w-lg text-4xl font-semibold tracking-tight text-pretty text-gray-950 sm:text-5xl">
+            <p className="mt-2 max-w-lg text-4xl font-semibold tracking-tight text-foreground text-pretty sm:text-5xl">
               Everything you need to deploy your app
             </p>
             <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
