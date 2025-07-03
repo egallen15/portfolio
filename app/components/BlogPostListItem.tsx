48ccbe42c47eed551db088dad699babe1d154432
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-07-03 10:31:17 -0500

    refactor: adjust size of blog post list images

--- app/components/BlogPostListItem.tsx
+++ app/components/BlogPostListItem.tsx
@@ -30,7 +30,7 @@ const BlogPostListItem: FC<BlogPostListItemProps> = ({
     <Link href={url} className="block rounded-lg hover:bg-gradient-to-tr hover:from-slate-200/30 hover:to-sky-100 dark:hover:bg-gradient-to-tr dark:hover:from-slate-400/30 dark:hover:to-sky-700/30">
       <div className="flex flex-col md:flex-row items-start w-full max-w-7xl p-4 rounded-lg">
         <div className="flex flex-shrink-0">
-          <Image src={imageUrl} alt="Post Image" width={500} height={150} className="object-cover aspect-video rounded-lg mb-4 lg:mb-0" />
+          <Image src={imageUrl} alt="Post Image" width={300} height={150} className="object-cover aspect-video rounded-lg mb-4 lg:mb-0" />
         </div>
         <div className="lg:ml-4">
             <div className="flex items-end">
