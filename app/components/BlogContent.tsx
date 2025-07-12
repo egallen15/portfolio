8628c91fcdd7a2afa69637242711a4c3f6204333
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-07-12 16:04:35 -0500

    fix: remove unnecessary margin adjustments in BlogContent and BlogPostListItem components

--- app/components/BlogContent.tsx
+++ app/components/BlogContent.tsx
@@ -9,7 +9,7 @@ const BlogContentClient = ({
 }: BlogContentProps) => {
   return (
     <div className="flex w-full max-w-7xl mx-auto">
-      <div className='w-full mx-6 xl:mx-0'>
+      <div className='w-full'>
         {showTitle && <h1 className="text-3xl md:text-5xl font-bold mb-6">{title}</h1>}
         {posts.length > 0 ? (
           <div className="flex flex-col space-y-4">
