48bc2e1003792a1d2dbbfaa1593a4e16d41bce65
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-07-28 14:18:16 -0500

    fix: update background color of AuthorBio component for improved aesthetics

--- app/components/AuthorBio.tsx
+++ app/components/AuthorBio.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const AuthorBio: React.FC = () => {
   return (
-    <div className="text-foreground bg-slate-100 dark:bg-slate-800 p-5 mx-6 max-w-3xl lg:items-center rounded-lg flex flex-col justify-center md:flex-row md:space-x-6 md:space-y-0 space-y-4 lg:mx-auto">
+    <div className="text-foreground bg-[#F4F6FD] dark:bg-slate-800 p-5 mx-6 max-w-3xl lg:items-center rounded-lg flex flex-col justify-center md:flex-row md:space-x-6 md:space-y-0 space-y-4 lg:mx-auto">
       <div className="flex-shrink-0">
         <Image
           className="rounded-full"
