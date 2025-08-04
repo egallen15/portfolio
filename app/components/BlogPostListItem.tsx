a21829ae57936ef790f8521c0de0f1c3da19af78
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-08-04 09:44:37 -0500

    fix: font tweaks for blog post items

--- app/components/BlogPostListItem.tsx
+++ app/components/BlogPostListItem.tsx
@@ -32,16 +32,16 @@ const BlogPostListItem: FC<BlogPostListItemProps> = ({
       href={url}
       className="block rounded-lg -mx-4 p-4 hover:bg-gradient-to-tr hover:from-slate-200/30 hover:to-sky-100 dark:hover:bg-gradient-to-tr dark:hover:from-slate-400/30 dark:hover:to-sky-700/30"
     >
-      <div className="flex flex-col md:flex-row items-start w-full max-w-7xl rounded-lg">
+      <div className="flex flex-col md:flex-row md:items-center w-full max-w-7xl rounded-lg">
         <div className="flex flex-shrink-0">
           <Image src={imageUrl} alt="Post Image" width={200} height={200} className="object-cover aspect-video rounded-lg mr-4 mb-4 lg:mb-0" />
         </div>
         <div className="w-fit">
           <div className="flex items-end">
-            <h2 className="text-lg font-bold mr-2">{title}</h2>
+            <h2 className="text-md font-semibold mr-2">{title}</h2>
           </div>
           <div className="h-[1px] w-full bg-gradient-to-r from-sky-400 to-sky-300 rounded-full my-2" />
-          <p className="text-md text-slate-700 dark:text-slate-300">{excerpt}</p>
+          <p className="text-sm text-slate-700 dark:text-slate-300">{excerpt}</p>
           <div className="mt-3 flex flex-row items-center">
             <p className="text-slate-500 text-sm font-light dark:text-slate-400 mr-3">
               {relativeDate}
