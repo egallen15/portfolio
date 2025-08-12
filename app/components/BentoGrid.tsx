7df61f6390b43ee8ba8cfef5a20710e17d0bfb61
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-08-12 17:30:49 -0500

    style: update bentogrid

--- app/components/BentoGrid.tsx
+++ app/components/BentoGrid.tsx
@@ -1,117 +1,134 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function BentoGrid() {
   return (
-        <div>
-          <div className="mx-auto xl:px-0 max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-            <p className="max-w-lg text-4xl font-bold text-foreground sm:text-5xl">
-              Portfolio
-            </p>
-            <div className="mt-8 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
-              <div className="relative lg:col-span-3">
-                <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]" />
-                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(2rem+1px)]">
-                  <Image
-                    alt=""
-                    width={1872}
-                    height={960}
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-performance.png"
-                    className="h-80 object-cover object-left"
-                  />
-                  <div className="p-10 pt-4">
-                    <h3 className="text-sm/4 font-semibold text-indigo-600">Performance</h3>
-                    <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Lightning-fast builds</p>
-                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida justo et nulla efficitur, maximus
-                      egestas sem pellentesque.
-                    </p>
-                  </div>
-                </div>
-                <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]" />
+    <div className="">
+      <div className="xl:mx-auto max-w-2xl lg:max-w-7xl">
+      
+        <div className="mt-6 grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-slate-50 lg:rounded-l-[2rem] dark:bg-slate-800" />
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-slate-950 max-lg:text-center dark:text-white">
+                  Mobile friendly
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-slate-600 max-lg:text-center dark:text-slate-400">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                  qui lorem cupidatat commodo.
+                </p>
               </div>
-              <div className="relative lg:col-span-3">
-                <div className="absolute inset-px rounded-lg bg-white lg:rounded-tr-[2rem]" />
-                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-tr-[calc(2rem+1px)]">
+              <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-slate-700 bg-slate-900 shadow-2xl dark:shadow-none dark:outline dark:outline-1 dark:outline-white/20">
                   <Image
                     alt=""
-                    width={1872}
-                    height={960}
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-releases.png"
-                    className="h-80 object-cover object-left lg:object-right"
+                    width={500}
+                    height={500}
+                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
+                    className="size-full object-cover object-top"
                   />
-                  <div className="p-10 pt-4">
-                    <h3 className="text-sm/4 font-semibold text-indigo-600">Releases</h3>
-                    <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Push to deploy</p>
-                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-                      Curabitur auctor, ex quis auctor venenatis, eros arcu rhoncus massa, laoreet dapibus ex elit vitae
-                      odio.
-                    </p>
-                  </div>
                 </div>
-                <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 lg:rounded-tr-[2rem]" />
-              </div>
-              <div className="relative lg:col-span-2">
-                <div className="absolute inset-px rounded-lg bg-white lg:rounded-bl-[2rem]" />
-                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-bl-[calc(2rem+1px)]">
-                  <Image
-                    alt=""
-                    width={1872}
-                    height={960}
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-speed.png"
-                    className="h-80 object-cover object-left"
-                  />
-                  <div className="p-10 pt-4">
-                    <h3 className="text-sm/4 font-semibold text-indigo-600">Speed</h3>
-                    <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Built for power users</p>
-                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-                      Sed congue eros non finibus molestie. Vestibulum euismod augue.
-                    </p>
-                  </div>
-                </div>
-                <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 lg:rounded-bl-[2rem]" />
-              </div>
-              <div className="relative lg:col-span-2">
-                <div className="absolute inset-px rounded-lg bg-white" />
-                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-                  <Image
-                    alt=""
-                    width={1872}
-                    height={960}
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-integrations.png"
-                    className="h-80 object-cover"
-                  />
-                  <div className="p-10 pt-4">
-                    <h3 className="text-sm/4 font-semibold text-indigo-600">Integrations</h3>
-                    <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Connect your favorite tools</p>
-                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-                      Maecenas at augue sed elit dictum vulputate, in nisi aliquam maximus arcu.
-                    </p>
-                  </div>
-                </div>
-                <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5" />
-              </div>
-              <div className="relative lg:col-span-2">
-                <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
-                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)]">
-                  <Image
-                    alt=""
-                    width={1872}
-                    height={960}
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-network.png"
-                    className="h-80 object-cover"
-                  />
-                  <div className="p-10 pt-4">
-                    <h3 className="text-sm/4 font-semibold text-indigo-600">Network</h3>
-                    <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Globally distributed CDN</p>
-                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-                      Aenean vulputate justo commodo auctor vehicula in malesuada semper.
-                    </p>
-                  </div>
-                </div>
-                <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
               </div>
             </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow outline outline-1 outline-black/5 lg:rounded-l-[2rem] dark:outline-white/15" />
+          </div>
+          <div className="relative max-lg:row-start-1">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem] dark:bg-slate-800" />
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-slate-950 max-lg:text-center dark:text-white">
+                  Performance
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-slate-600 max-lg:text-center dark:text-slate-400">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit
+                  maiores impedit.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                <Image
+                  alt=""
+                  src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-performance.png"
+                  className="w-full max-lg:max-w-xs dark:hidden"
+                  width={500}
+                  height={500}
+                />
+                <Image
+                  alt=""
+
+                  src="https://tailwindcss.com/plus-assets/img/component-images/dark-bento-03-performance.png"
+                  className="hidden w-full max-lg:max-w-xs dark:block"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow outline outline-1 outline-black/5 max-lg:rounded-t-[2rem] dark:outline-white/15" />
+          </div>
+          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+            <div className="absolute inset-px rounded-lg bg-white dark:bg-slate-800" />
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-slate-950 max-lg:text-center dark:text-white">
+                  Security
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-slate-600 max-lg:text-center dark:text-slate-400">
+                  Morbi viverra dui mi arcu sed. Tellus semper adipiscing
+                  suspendisse semper morbi.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
+                <Image
+                  alt=""
+                  src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-security.png"
+                  className="h-[min(152px,40cqw)] object-cover dark:hidden"
+                  width={500}
+                  height={500}
+                />
+                <Image
+                  alt=""
+                  src="https://tailwindcss.com/plus-assets/img/component-images/dark-bento-03-security.png"
+                  className="hidden h-[min(152px,40cqw)] object-cover dark:block"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow outline outline-1 outline-black/5 dark:outline-white/15" />
+          </div>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem] dark:bg-slate-800" />
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-slate-950 max-lg:text-center dark:text-white">
+                  Powerful APIs
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-slate-600 max-lg:text-center dark:text-slate-400">
+                  Sit quis amet rutrum tellus ullamcorper ultricies libero dolor
+                  eget sem sodales gravida.
+                </p>
+              </div>
+              <div className="relative min-h-[30rem] w-full grow">
+                <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-slate-900 shadow-2xl outline outline-1 outline-white/10 dark:bg-slate-900/60 dark:shadow-none">
+                  <div className="flex bg-slate-900 outline outline-1 outline-white/5">
+                    <div className="-mb-px flex text-sm/6 font-medium text-slate-400">
+                      <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                        NotificationSetting.jsx
+                      </div>
+                      <div className="border-r border-slate-600/10 px-4 py-2">
+                        App.jsx
+                      </div>
+                    </div>
+                  </div>
+                  <div className="px-6 pb-14 pt-6">
+                    {/* Your code example */}asdfasdfadf
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow outline outline-1 outline-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem] dark:outline-white/15" />
           </div>
         </div>
-      )
-    }
+      </div>
+    </div>
+  );
+}
