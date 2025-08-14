49499cffdffa555675cd70994601ae73c457023a
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-08-14 08:09:43 -0500

    fix: update to use nextjs Link

--- app/components/Breadcrumb.tsx
+++ app/components/Breadcrumb.tsx
@@ -1,4 +1,5 @@
 import { HomeIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link'
 
 interface BreadcrumbPage {
   name: string
@@ -17,10 +18,10 @@ export default function Breadcrumb({ pages, homeHref = "/" }: BreadcrumbProps) {
       <ol role="list" className="flex items-center space-x-4">
         <li>
           <div>
-            <a href={homeHref} className="text-gray-400 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-300">
+            <Link href={homeHref} className="text-gray-400 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-300">
               <HomeIcon aria-hidden="true" className="size-5 shrink-0" />
               <span className="sr-only">Home</span>
-            </a>
+            </Link>
           </div>
         </li>
         {pages.map((page) => (
@@ -34,13 +35,13 @@ export default function Breadcrumb({ pages, homeHref = "/" }: BreadcrumbProps) {
               >
                 <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
               </svg>
-              <a
+              <Link
                 href={page.href}
                 aria-current={page.current ? 'page' : undefined}
                 className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
               >
                 {page.name}
-              </a>
+              </Link>
             </div>
           </li>
         ))}
