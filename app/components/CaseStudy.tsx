82018bac9ef0492c1e256d8d150c770b8f40f915
Eric Allen <31082008+egallen15@users.noreply.github.com>
2025-08-14 12:55:55 -0500

    style: case study layout tweaks

--- app/components/CaseStudy.tsx
+++ app/components/CaseStudy.tsx
@@ -82,13 +82,13 @@ export default function CaseStudy({
             </div>
           </div>
         </div>
-        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-24 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <Image
             alt={image.alt}
             src={image.src}
             width={image.width}
             height={image.height}
-            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] dark:bg-gray-800 dark:ring-white/10"
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] dark:ring-white/10"
           />
         </div>
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
