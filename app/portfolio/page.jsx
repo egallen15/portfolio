ad1e81a862d91772a6b28dac638426f336257e93
Eric Allen <egallen15@gmail.com>
2025-09-28 08:36:58 -0500

    content: add title and description to portfolio page

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -7,6 +7,10 @@ const PortfolioPage = () => {
             <div className="w-full max-w-7xl xl:mx-0">
                 <Breadcrumb pages={[{ name: "Portfolio", href: "/portfolio", current: true }]}  />
             </div>
+            <h1 className="text-3xl md:text-5xl font-bold mb-6">My work</h1>
+            <span className="text-xl text-slate-800 dark:text-slate-400 mb-8">
+                A few interesting projects I've worked on in my career so far.
+            </span>
             <BentoGrid />
         </main>
     );
